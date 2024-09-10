6568cf81e87e86412da3015689b4058f4bb5b3af
ABDUL FADIGA <tardzenyuyofficial@gmail.com>
2024-09-11 00:25:50 +0100

    added user join signal

--- Public/js/chat.js
+++ Public/js/chat.js
@@ -3,6 +3,11 @@ const socket = io();
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const message = document.getElementById('message')
+const currentTime = new Date().toLocaleTimeString();
+
+
+document.getElementById("messages2").innerHTML = `<p class="joined">You joined at ${currentTime}</p>`;
+socket.emit('new-user', name)
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -21,3 +26,14 @@ socket.on('chat message', (msg) => {
     window.scrollTo(0, document.body.scrollHeight)
 
 })
+
+socket.on('chat message', (msg) => {
+    console.log("msgs : ", msg)
+    // i.e You : hello how are  u
+    let user = msg.name == name ? "You" : msg.name
+
+
+    const item = (msg.name == name) ? `<p class="chat-message right">You : ${msg.msg} <span class="timeStamp">${currentTime}</span>` : `<p class="chat-message">${msg.name} : ${msg.msg} <span class="timeStamp">${currentTime}</span>`
+    document.getElementById("messages").innerHTML += item;
+    console.log(item)
+});
